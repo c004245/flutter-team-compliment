3eae9d9c7e9a85828439d1b8c820941d54bbb2aa
mapppppppp <c004245@naver.com>
2023-01-05 17:44:20 +0900

    chore: set login controller

--- lib/login/login_controller.dart
+++ lib/login/login_controller.dart
@@ -1,9 +1,30 @@
 import 'package:get/get.dart';
 
 import 'package:flutter_team_compliment/common/app_routes.dart';
+import 'package:flutter_team_compliment/data/service/authentication_service.dart';
 
 class LoginController extends GetxController {
-  LoginController();
 
+  final AuthenticationService _authenticationService;
+
+
+  LoginController(this._authenticationService);
+
+  void signInWithGoogle() {
+    _authenticationService
+      .signInWithGoogle()
+        .then((uid) => _signInComplete(uid))
+        .onError((error, stackTrace) => _signInOnError());
+  }
+
+  void _signInComplete(String? uid) {
+    uid != null
+        ? Get.offAllNamed(AppRoutes.mainList)
+        : Get.offAll(AppRoutes.login);
+  }
+
+  void _signInOnError() {
+    //내부데이터를 제거하고 로그인 화면으로넘긴다..
+  }
 
 }
